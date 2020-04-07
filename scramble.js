1aa285b2e105ed96e617c7a4094bfbaf77c60c25
alex haszard <alex.haszard@gmail.com>
2020-04-08 00:42:46 +1200

    comments
    commenting and removing useless code

--- scramble.js
+++ scramble.js
@@ -1,21 +1,21 @@
 function scramble() {
   
   this.show = function(txt) {
-    textAlign("center");
+    textAlign("center"); //setup the text for the scramble
     textSize(windowWidth / 30);
     text(txt, width/2, windowHeight / 12);
   }
   
   this.genScram = function() {
-    let scram = [];
-    let final = "";
-    let prevMove = 0;
+    let scram = []; //create an array for scramble values
+    let final = ""; //what will be returned at the end
+    let prevMove = 0; //the previous move that was generated to stop redundant moves from being created
     for (let i = 0; i < 20; i++) {
-      let ran = Math.floor(Math.random() * 12) + 1;
-      if (ran == 1 && prevMove != 1 && prevMove != 2) {
-        scram[i] = "U";
-        prevMove = 1;
-      } else if (ran == 2 && prevMove != 2 && prevMove != 1) {
+      let ran = Math.floor(Math.random() * 6) + 1; //generate a random move from 1-6
+      if (ran == 1 && prevMove != 1 && prevMove != 2) { //if the move is not the same as the previous move and one that would make it redundant eg U and D moves
+        scram[i] = "U"; //then add the move to the array
+        prevMove = 1; //so that no moves will be redundant
+      } else if (ran == 2 && prevMove != 2 && prevMove != 1) { //this is repeated 6 times
         scram[i] = "D";
         prevMove = 2;
       } else if (ran == 3 && prevMove != 3 && prevMove != 4) {
@@ -31,19 +31,19 @@ function scramble() {
         scram[i] = "B";
         prevMove = 6;
       }else {
-        i--;
+        i--; //if there is no move that is viable (redundant moves) then reset the loop and make a new move in its place
       }
     }
     for (let i = 0; i < 20; i++) {
-      let prime = Math.floor(Math.random() * 3) ;
+      let prime = Math.floor(Math.random() * 3) ; //generate a random number 1-3 for normal, prime and double moves
       let pri = "";
       if (prime == 1) {
-        pri = "'";
+        pri = "'"; //if the number is 1 then it is a prime move
       } else if (prime == 2) {
-		pri = "2";
+		pri = "2"; //if the number is 2 then it is a double move
 	  }
-      final += scram[i] + pri + " ";
+      final += scram[i] + pri + " "; //output the array in a string with the prime and double moves
     }
-    return final;
+    return final; 
   }
 }
--- sketch.js
+++ sketch.js
@@ -1,20 +1,19 @@
-let counter = 0;
-let new_counter;
-let outputc;
-let seconds = 0;
-let new_seconds;
-let minutes = 0;
-let timerStarted = false;
-let keyStopped = false;
-let justSolved = false;
-let scram1;
-let current_time;
+let counter = 0; //counts the time
+let new_counter; //the counter reformatted to be displayed
+let seconds = 0; //the seconds
+let new_seconds; //seconds reformatted
+let minutes = 0; //the minutes, they do not need to be reformatted (yet)
+let timerStarted = false; //tell the draw loop whether or not the timer has started
+let keyStopped = false; //used to stop the timer from starting itself when it is stopped
+let justSolved = false; //used to tell the draw loop to log the time in console
+let scram1; //the scramble
+let current_time; //the time formatted
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   setInterval(timer, 10);
-  sc = new scramble();
-  scram1 = sc.genScram();
+  sc = new scramble();  //create the scramble function
+  scram1 = sc.genScram(); //generate first scramble
 }
 
 function draw() {
@@ -23,82 +22,73 @@ function draw() {
   textFont("Arial");
   textSize(windowWidth / 8);
   textAlign("center");
-  new_seconds = String(seconds).padStart(2, '0');
-  new_counter = String(counter).padStart(2, '0');
-  outputc = String(counter + 1).padStart(2, '0');
+  new_seconds = String(seconds).padStart(2, '0'); //make the seconds always have 2 digits
+  new_counter = String(counter).padStart(2, '0'); //make the counters always have 2 digits
   if (minutes > 0) {
-	current_time = minutes + ":" + new_seconds + "." + new_counter;
+	current_time = minutes + ":" + new_seconds + "." + new_counter; //output the current time with minutes if there has been more than one minute
     text(current_time, width / 2, height / 2);
   } else {
 	current_time = seconds + "." + new_counter;
-    text(current_time, width / 2, height / 2 + windowWidth / 30);
+    text(current_time, width / 2, height / 2 + windowWidth / 30); //output the current time without minutes if it has not been a minute yet
   }
   if (justSolved == true) {
-	console.log(current_time);
-	justSolved = false;
+	console.log(current_time); //log the previous time when it has been solved
+	justSolved = false; //stop the if statement from being called again
   }
-  sc.show(scram1);
+  sc.show(scram1); //show the scramble text
 }
 
 function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
+  resizeCanvas(windowWidth, windowHeight); //change the canvas size automatically
 }
 
 function timer() {
   if (keyStopped == false && timerStarted == true) {
-    counter++;
+    counter++; //count up if the timer is going
   }
   if ((counter + 1) % 101 == 0) {
-    counter = 0;
-    seconds++;
+    counter = 0; //if the counter is at 100 then put it back to 0
+    seconds++; //and then increase seconds by 1
   }
   if ((seconds + 1) % 61 == 0) {
-    seconds = 0;
-    minutes++;
+    seconds = 0; //if it has been 60 seconds then put seconds to 0
+    minutes++; //and increase minutes by 1
   }
 }
 
 function keyPressed() {
-  if (key === ' ') {
+  if (key === ' ') { //if the key is spacebar
     if (timerStarted == true) {
-      keyStopped = true;
-      scram1 = sc.genScram();
-	  justSolved = true;
+      keyStopped = true; //if the timer is started then stop the timer
+      scram1 = sc.genScram(); //generate a new scramble
+	  justSolved = true; //tell the draw loop to output the solve to the console
     }
   }
 }
 
-function touchStarted() {
+function touchStarted() { //same as keyPressed for mobile
   if (timerStarted == true) {
-    keyStopped = true;
+    keyStopped = true; 
     scram1 = sc.genScram();  
 	justSolved = true;
   }
 }
 
 function keyReleased() {
-  if (key === ' ') {
+  if (key === ' ') { //if the key is spacebar
     if (timerStarted == false) {
-      timerStarted = true;
-      keyStopped = false;
-      counter = 0;
+      timerStarted = true; //if the timer is not started start the timer
+      keyStopped = false; //when the key is pressed to stop the timer this is used to make sure that is will not start again when the key is released
+      counter = 0; //reset the timer
       seconds = 0;
       minutes = 0;
     } else {
-      timerStarted = false;
+      timerStarted = false; //stop the timer
     }
   }
 }
 
-function mousePressed() {
-  return;
-}
-
-function mouseReleased() {
-  return;
-}
-
-function touchEnded() {
+function touchEnded() { //same as keyReleased for mobile
   if (timerStarted == false) {
     timerStarted = true;
     keyStopped = false;
@@ -108,4 +98,12 @@ function touchEnded() {
   } else {
     timerStarted = false;
   }
+}
+
+function mousePressed() { //the mobile controls are automatically bound to mouse to these are to disable that functionality
+  return;
+}
+
+function mouseReleased() {
+  return;
 }
