6b6c9cec944badd6e281f36752872714e263fa16
alexthehaszard <aggis.haszard@gmail.com>
2020-06-29 09:50:17 +1200

    change to new website
    change the entire thing to use a new main site that just looks nicer.

---
+++ sketch.js
@@ -0,0 +1,27 @@
+const textToShow = "lexthehaszard";
+let textShowing = "a";
+let index = 0;
+
+function setup() {
+  noCanvas();
+  frameRate(10);
+}
+
+function draw() {
+  updateText();
+}
+
+function updateText() {
+  if (textToShow[index] === undefined) {
+    index++;
+    if (document.getElementById("outputText").innerHTML === textShowing + "_" && index % 20 === 10) {
+      document.getElementById("outputText").innerHTML = textShowing;
+    } else if (index % 20 === 0) {
+      document.getElementById("outputText").innerHTML = textShowing + "_";
+    }
+    return;
+  }
+  textShowing += textToShow[index];
+  index++;
+  document.getElementById("outputText").innerHTML = textShowing + "_";
+}
