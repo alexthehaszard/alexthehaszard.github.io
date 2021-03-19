ad98c1530fb8e88e8388c5ae4853762e1502b47b
alexthehaszard <aggis.haszard@gmail.com>
2021-03-19 19:17:46 +1300

    ❌ - remove poigen

--- poiGen/sketch.js
+++
@@ -1,8 +0,0 @@
-const poi = ["Shark", "Pleasant", "Craggy", "Steamy", "Yacht", "Sweaty", "Risky", "Frenzy", "Dirty", "Agency", "Holly", "Weeping", "Lazy", "Retail", "Grotto", "Slurpy", "Misty", "Rig"];
-let currentPoi = "Press button for POI"
-
-function buttonPressed() {
-  let index = Math.floor(Math.random() * poi.length)
-  currentPoi = poi[index]
-  document.getElementById("poi").innerHTML = currentPoi
-}
